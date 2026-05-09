69bed3bf1e20b0dd577b17e0e37c1582b6c82f2f
Bananutzul <zamhoria15@gmail.com>
2026-05-09 14:55:33 +0300

    Created Arrow header

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Friday Night Funkin");
+
+    while (window.isOpen()) {
+        while (const auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}

06209fab3a4dd79bd4cb7c14b956b3fbe6b2634c
Bananutzul <zamhoria15@gmail.com>
2026-05-16 14:33:29 +0300

    Started Menu class

---
+++ Menu.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+#include "GameManager.h"
+#include "Chart.h"
+
+using namespace std;
+
+class Menu {
+private:
+    Chartc chart;
+    GameManager gm;
+public:
+
+};
